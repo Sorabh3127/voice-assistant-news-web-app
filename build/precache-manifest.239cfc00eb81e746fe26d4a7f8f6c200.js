d677a7e3be462fc17e4db55ed73ec36b177d60ee
Nishant Rai <nishantrai11@outlook.com>
2022-11-10 22:50:42 +0530

    initial commit

---
+++ build/precache-manifest.239cfc00eb81e746fe26d4a7f8f6c200.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e0df659cac407844eaebb0903a2229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe01c7a4d58d06da52b",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "ae8997939b3d414e0522",
+    "url": "/static/js/2.c0fef7a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c0fef7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe01c7a4d58d06da52b",
+    "url": "/static/js/main.ef7cb9b7.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
